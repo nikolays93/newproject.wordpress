3afe53093aec5ec5c2ab5bbce1aa6a0b5ee8211e
NikolayS93 <nikolays93@ya.ru>
2020-08-15 15:49:46 +0400

    Clear waste

--- www/html/wp-content/themes/project/footer.php
+++ www/html/wp-content/themes/project/footer.php
@@ -9,29 +9,6 @@
  * @version 1.0
  */
 ?>
-			<div class="slider row justify-content-between mt-5">
-				<div class="col">
-					<a href="<?= TPL ?>/img/placeholder.png" data-fancybox="gal1"><img
-						src="<?= TPL ?>/img/placeholder.png" width="150" class="ac"></a>
-				</div>
-				<div class="col">
-					<a href="<?= TPL ?>/img/placeholder.png" data-fancybox="gal1"><img
-						src="<?= TPL ?>/img/placeholder.png" width="150" class="ac"></a>
-				</div>
-				<div class="col">
-					<a href="<?= TPL ?>/img/placeholder.png" data-fancybox="gal1"><img
-						src="<?= TPL ?>/img/placeholder.png" width="150" class="ac"></a>
-				</div>
-				<div class="col">
-					<a href="<?= TPL ?>/img/placeholder.png" data-fancybox="gal1"><img
-						src="<?= TPL ?>/img/placeholder.png" width="150" class="ac"></a>
-				</div>
-				<div class="col">
-					<a href="<?= TPL ?>/img/placeholder.png" data-fancybox="gal1"><img
-						src="<?= TPL ?>/img/placeholder.png" width="150" class="ac"></a>
-				</div>
-			</div>
-		</div><!-- .container -->
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
